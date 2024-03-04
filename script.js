c2d568a90a37fdb1c15a6386a3e77c392fed11a2
Jazmine Tran <jazminetra00@gmail.com>
2024-03-04 10:25:39 -0600

    defined functions and event listeners

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden'); // you can pass more arguments in this by adding a comma
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
