ac896caee9761f64a1ece34bfc655c295e3ccfcf
Jazmine Tran <jazminetra00@gmail.com>
2024-03-04 10:36:03 -0600

    escape key functional to close modal

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-  console.log('Button clicked');
   modal.classList.remove('hidden'); // you can pass more arguments in this by adding a comma
   overlay.classList.remove('hidden');
 };
@@ -20,5 +19,11 @@ for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
-
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
